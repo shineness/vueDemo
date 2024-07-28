36f9bf91b48f6704a118c1bcd378f3926d293aa3
shine <489548491@qq.com>
2024-07-28 17:41:53 +0800

    first commit

---
+++ src/components/TestApp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <h1>testPage</h1>
+    <p>count{{ count }}</p>
+    <p>doubleCount{{ doubleCount }}</p>
+    <button class="bt" type="button" @click="increment">add</button>
+    <p ref="hello">count{{ count }}</p>
+  </div>
+</template>
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+import { getCurrentInstance, ref, onMounted } from 'vue'
+
+const { proxy } = getCurrentInstance()
+const store = useCounterStore()
+const { count, doubleCount } = storeToRefs(store)
+const { increment } = store
+onMounted(() => {
+  console.log(proxy.$refs.hello)
+})
+</script>
+<style scoped lang="less">
+.main {
+  width: 200px;
+  .bt {
+    font-size: 30px;
+  }
+}
+</style>
